4bd77f64678aab77a6905ec2d730152e492f8322
Joao010 <joaopedrocz12345@gmail.com>
2022-03-13 06:56:10 -0300

    feat(AddPerson, GetPerson, GetPersons, UpdatePerson, DeletePerson): ⭐ add first route & add some features

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,17 +1,54 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
-    private static final List<Person> DB = new ArrayList<>();
+
+    private static List<Person> DB = new ArrayList<>();
 
     @Override
-    public int insertPerson(UUID id, Person person) {
-        DB.add(new Person(id, person.getName()));
-        return 1;
+    public Person insertPerson(UUID id, Person person) {
+        Person data = new Person(id, person.getName());
+        DB.add(data);
+        return new Person(
+            id, person.getName()
+        );
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public void deletePersonById(UUID id) {
+        Optional<Person> personMaybe = selectPersonById(id);
+        if(personMaybe.isEmpty()) return;
+        DB.remove(personMaybe.get());
+    }
+
+    @Override
+    public void updatePersonById(UUID id, Person person) {
+        Person originalData = selectPersonById(id).orElse(null);
+        if(originalData == null) return;
+        Person updatedData = new Person(id, person.getName());
+        int indexOfOriginalData = DB.indexOf(originalData);
+        if(indexOfOriginalData >= 0) {
+            DB.set(indexOfOriginalData, updatedData);
+        }
     }
 }
--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -2,13 +2,23 @@ package com.example.demo.dao;
 
 import com.example.demo.model.Person;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
-    int insertPerson(UUID id, Person person);
+    Person insertPerson(UUID id, Person person);
 
-    default int insertPerson(Person person) {
+    default Person insertPerson(Person person) {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    void deletePersonById(UUID id);
+
+    void updatePersonById(UUID id, Person person);
 }
